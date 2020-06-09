70f891ab1ae9eba62dd997e75ccf8ac39d4341ac
andreasoprisan <oprisanandreas@gmail.com>
2020-06-09 17:23:03 +0300

    2.5.1 -prettified tester.py

--- tester.py
+++ tester.py
@@ -1,5 +1,5 @@
 from helpers import singleSudokuSolver
-import pandas as pd 
+import pandas as pd
 import time
 
 input_string = '400870090092000487078090100060059748980040000705680930009020810817900250000018079'
@@ -7,11 +7,11 @@ array = singleSudokuSolver.array_input(input_string)
 
 
 start = time.time()
-chunksize = 10000
+chunksize = 1000
 for chunk in pd.read_csv("data/sudoku.csv", chunksize=chunksize, nrows=10000):
-    
 
-    print(pd.value_counts(chunk['solutions'].eq(chunk['quizzes'].apply(singleSudokuSolver.solve_quiz))))
+    print(pd.value_counts(chunk['solutions'].eq(
+        chunk['quizzes'].apply(singleSudokuSolver.solve_quiz))))
 
 end = time.time()
 print(end-start)
